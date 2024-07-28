626aca9788967153794769a4196857db4e841c64
hardik <hardik190103@outlook.com>
2024-07-28 07:08:30 +0530

    added validators

--- models/expenseModel.js
+++ models/expenseModel.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const expenseSchema = new mongoose.Schema({
   description: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   amount: {
     type: Number,
-    required: true
+    required: true,
+    min: [0, 'Amount must be positive']
   },
   splitMethod: {
     type: String,
@@ -22,10 +24,25 @@ const expenseSchema = new mongoose.Schema({
         required: true
       },
       amountOwed: {
-        type: Number
+        type: Number,
+        min: [0, 'Amount owed must be positive'],
+        validate: {
+          validator: function(v) {
+            return this.splitMethod !== 'percentage' || v === undefined;
+          },
+          message: 'Amount owed should be undefined when split method is percentage'
+        }
       },
       percentageOwed: {
-        type: Number
+        type: Number,
+        min: [0, 'Percentage owed must be positive'],
+        max: [100, 'Percentage owed cannot exceed 100'],
+        validate: {
+          validator: function(v) {
+            return this.splitMethod !== 'exact' || v === undefined;
+          },
+          message: 'Percentage owed should be undefined when split method is exact'
+        }
       }
     }
   ],
@@ -33,8 +50,9 @@ const expenseSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'User', 
     required: true 
- }
+  }
+}, { 
+  timestamps: true 
 });
 
 module.exports = mongoose.model('Expense', expenseSchema);
-
--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,31 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    mobile: { type: String, required: true }
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Invalid email address']
+    },
+    name: { 
+        type: String, 
+        required: true,
+        trim: true
+    },
+    mobile: { 
+        type: String, 
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /\d{10}/.test(v);
+            },
+            message: props => `${props.value} is not a valid mobile number!`
+        }
+    }
+}, { 
+    timestamps: true 
 });
 
 module.exports = mongoose.model('User', userSchema);
